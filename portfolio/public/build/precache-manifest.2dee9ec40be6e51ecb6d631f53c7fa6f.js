5c95615020851b23840a4e24b548cc59604606ec
alex-waite <alexanderconorwaite@gmail.com>
2020-07-15 20:31:52 +0930

    yoooo

---
+++ portfolio/public/build/precache-manifest.2dee9ec40be6e51ecb6d631f53c7fa6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b472e32dd0f5e3f2d3229cb8c14e238c",
+    "url": "/plsdeployreact/index.html"
+  },
+  {
+    "revision": "9804cf739d6ccd86d030",
+    "url": "/plsdeployreact/static/css/main.12e47319.chunk.css"
+  },
+  {
+    "revision": "bc94675e51f4436d6300",
+    "url": "/plsdeployreact/static/js/2.21784932.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/plsdeployreact/static/js/2.21784932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9804cf739d6ccd86d030",
+    "url": "/plsdeployreact/static/js/main.70d6d4ce.chunk.js"
+  },
+  {
+    "revision": "18cbf74c6d5fdd9ba87c",
+    "url": "/plsdeployreact/static/js/runtime-main.5889f9cf.js"
+  }
+]);
