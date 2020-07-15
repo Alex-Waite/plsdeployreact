1fdd7c867239cf808d814767634f4e43597ea15e
alex-waite <alexanderconorwaite@gmail.com>
2020-07-15 20:25:05 +0930

    hmm

---
+++ src/pages/Aboutme.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Aboutme () {
+    return (
+        <main>
+        <section class="section-1">
+            <div class="container text-center">
+                <div class="row">
+                    
+                    
+                </div>
+
+            </div>
+        </section>
+    </main>
+    )
+}
+
+export default Aboutme
